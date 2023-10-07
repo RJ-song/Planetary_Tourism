597169b439ddeeed1b3a4ee2b4bd6296724bfb3b
orertrr <orertrr6207@gmail.com>
2023-10-07 23:38:55 +0800

    Add solar-system
    Signed-off-by: orertrr <orertrr6207@gmail.com>

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './Home.js';
 import Moon from './planets/moon.js';
+import SolarSystem from './Pages/SolarSystem';
+
 function App() {
   return (
     <div>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/Moon' element={<Moon />} />
+        <Route path='/SolarSystem' element={<SolarSystem />} />
       </Routes>
       
     </div>
