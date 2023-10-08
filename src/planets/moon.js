a64768ed84f73eae4905a245a077d97235178d78
wuxinde409 <wuxinde409.co@gmail.com>
2023-10-08 23:02:09 +0800

    1100

--- src/planets/moon.js
+++ src/planets/moon.js
@@ -2,6 +2,13 @@ import { Link } from 'react-router-dom';
 import Marsmodel from '../marsmodel';
 import * as THREE from "three";
 import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader.js";
+import Earthmodel from '../earthmodel';
+import Jupitermodel from '../jupitermodel';
+import Merucurymodel from '../mercurymodel';
+import Neptunemodel from '../neptunemodel';
+import Saturnmodel from '../saturnmodel';
+import Uranusmodel from '../uranusmodel';
+import Venussmodel from '../venusmodel';
 function Moon() {
     const moonContainerStyle = {
         width: '10%',      // 设置宽度
@@ -12,8 +19,11 @@ function Moon() {
 
     return (
         <div >
-            <div  className = 'container'style={moonContainerStyle}>
+            {/* <div  className = 'container'style={moonContainerStyle}>
                 <Marsmodel />
+            </div> */}
+            <div>
+                <Venussmodel/>
             </div>
             {/* 在这里添加其他内容 */}
         </div>
